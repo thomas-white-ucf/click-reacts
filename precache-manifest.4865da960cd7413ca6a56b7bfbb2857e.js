acc11150327dc9b25b41820d1aa1b00e1ee3711e
Thomas White <thomas.white.ucf@gmail.com>
2019-10-27 16:33:04 -0400

    Updates

---
+++ precache-manifest.4865da960cd7413ca6a56b7bfbb2857e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009f57a7f6053acaa0a5a274f240023d",
+    "url": "/click-reacts/index.html"
+  },
+  {
+    "revision": "041e0c6d8761bc49a857",
+    "url": "/click-reacts/static/css/main.acae3bab.chunk.css"
+  },
+  {
+    "revision": "65ef24d09dfa9373e3cd",
+    "url": "/click-reacts/static/js/2.4f2b1d22.chunk.js"
+  },
+  {
+    "revision": "041e0c6d8761bc49a857",
+    "url": "/click-reacts/static/js/main.f0b07201.chunk.js"
+  },
+  {
+    "revision": "2c70eb0cef74cef575b3",
+    "url": "/click-reacts/static/js/runtime-main.f907bce0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/click-reacts/precache-manifest.d65c7ac9db037c06da2880d32593ac37.js"
+  "/click-reacts/precache-manifest.4865da960cd7413ca6a56b7bfbb2857e.js"
 );
 
 self.addEventListener('message', (event) => {
